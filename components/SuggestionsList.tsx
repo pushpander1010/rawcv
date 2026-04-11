4126541c0d67116f56dd12118efd8a5cb5740f79
pushpander1010 <2023mt93337@wilp.bits-pilani.ac.in>
2026-04-11 20:55:04 +0530

    undo button

--- components/SuggestionsList.tsx
+++ components/SuggestionsList.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import React from "react";
+import React, { useState } from "react";
 import { useResume } from "@/context/ResumeContext";
 import type { Suggestion } from "@/types";
 
